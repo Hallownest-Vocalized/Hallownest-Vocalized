89ac4364e11ffcabe6be23be2a905e90d3bef6ed
TheMulhima <mulhima003@gmail.com>
2022-11-03 23:50:27 -0400

    remove unnecessary settings

--- HKVocal/Settings.cs
+++ HKVocal/Settings.cs
@@ -4,15 +4,13 @@ public class GlobalSettings
 {
     public int Volume = 10;
     public bool OrdealZoteSpeak = true;
-    public bool autoScroll = false; //TODO: test
-    public bool dnDialogue = false; //TODO: test
-    public bool scrollLock = false; //TODO: test
+    public bool autoScroll = false;
+    public bool dnDialogue = false;
+    public bool scrollLock = false;
 }
 public class SaveSettings
 {
     public int GrubConvo = -1;
-    public bool ZoteOn = true;
-    public bool UnlockedZoteOpt = false;
     public List<string> FinishedConvos = new List<string>();
     public Dictionary<string, int> PersistentVoiceActors = new Dictionary<string, int>();
 }
