f54a3894a0f527209a3d7f2ffc4f0ef0fad3978a
Zickles <76439587+Zickles@users.noreply.github.com>
2022-11-12 10:54:10 -0500

    paleflower stuff i forgot to commit

--- HKVocal/EasterEggs/PaleFlower.cs
+++ HKVocal/EasterEggs/PaleFlower.cs
@@ -1,34 +1,79 @@
 ﻿
+using Satchel;
+
 namespace HKVocals.EasterEggs
 {
     public static class PaleFlower
     {
         public static void Hook()
         {
-            On.HealthManager.TakeDamage += TakeDamage;
+            /*On.HealthManager.TakeDamage += TakeDamage;*/
+            On.HealthManager.Die += PaleLurkerDie;
             On.PlayMakerFSM.OnEnable += PaleLurkerFSM;
         }
 
-        private static void TakeDamage(On.HealthManager.orig_TakeDamage orig, HealthManager self, HitInstance hitInstance)
+
+        private static void PaleLurkerDie(On.HealthManager.orig_Die orig, HealthManager self, float? attackDirection, AttackTypes attackType, bool ignoreEvasion)
         {
-            if (self.gameObject.name == "Pale Lurker")
+            if (self.gameObject.name != "Pale Lurker")
             {
-                if (self.hp == 1)
-                {
-                    return;
-                }
-                else if (self.hp <= hitInstance.DamageDealt)
-                {
-                    self.hp = 1;
-                    PlayMakerFSM.BroadcastEvent("Pale Lurker 0 HP");
-                    return;
-                }
+                orig(self, attackDirection, attackType, ignoreEvasion);
             }
-            orig(self, hitInstance);
+            else if (self.gameObject.name == "Pale Lurker")
+            {
+                self.gameObject.AddComponent<PLMono>();
+                self.gameObject.GetComponent<tk2dSpriteAnimator>().Play("Idle");
+                self.gameObject.AddComponent<OnTriggerEnter>();
+                self.gameObject.layer = 13;
+
+                /*     self.gameObject.RemoveComponent<DamageHero>();
+                self.gameObject.RemoveComponent<HealthManager>();*/
+            }
+
         }
 
+        private class PLMono : MonoBehaviour
+        {
+            
+            private void Start()
+            {
+               gameObject.RemoveComponent<DamageHero>();
+               gameObject.RemoveComponent<HealthManager>();
+            }
 
-        private static void PaleLurkerFSM(On.PlayMakerFSM.orig_OnEnable orig, PlayMakerFSM self)
+            private void Update()
+            {
+                if (gameObject.GetComponent<Rigidbody2D>().velocity.y == 0f && gameObject.GetComponent<Rigidbody2D>().velocity.x == 0f)
+                {
+                    gameObject.layer = 13;
+                    gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                    gameObject.GetComponent<Rigidbody2D>().gravityScale = 0f;
+                    gameObject.GetComponent<Collider2D>().isTrigger = true;
+                }
+            }
+
+        }
+
+        /*        private static void TakeDamage(On.HealthManager.orig_TakeDamage orig, HealthManager self, HitInstance hitInstance)
+                {
+                    if (self.gameObject.name == "Pale Lurker")
+                    {
+                        if (self.hp == 1)
+                        {
+                            return;
+                        }
+                        else if (self.hp <= hitInstance.DamageDealt)
+                        {
+                            self.hp = 1;
+                            *//*PlayMakerFSM.BroadcastEvent("Pale Lurker 0 HP");*//*
+                            return;
+                        }
+                    }
+                    orig(self, hitInstance);
+                }*/
+
+
+        public static void PaleLurkerFSM(On.PlayMakerFSM.orig_OnEnable orig, PlayMakerFSM self)
         {
             orig(self);
 
@@ -36,8 +81,12 @@ namespace HKVocals.EasterEggs
             if (self.gameObject.scene.name == "GG_Lurker" && self.FsmName == "Lurker Control" && self.gameObject.name == "Pale Lurker")
             {
                 GameManager.instance.transform.Find("_GameManager/AudioManager/Music/Action").gameObject.SetActive(false);
-                self.AddFsmState("Dead");
-                self.AddGlobalTransition("Pale Lurker 0 HP", "Dead");
+
+                
+
+/*                self.AddFsmState("Dead");
+                *//*self.AddGlobalTransition("Pale Lurker 0 HP", "Dead");*//*
+                self.gameObject.LocateMyFSM("Lurker Control").SetState("Dead");
 
                 self.AddFsmAction("Dead", new Tk2dPlayAnimation()
                 {
@@ -47,9 +96,13 @@ namespace HKVocals.EasterEggs
                     },
                     animLibName = "",
                     clipName = "Idle"
-                });
+                }); */
             }
         }
 
+        private class OnTriggerEnter : MonoBehaviour
+        {
+
+        }
     }
-}
+    }
