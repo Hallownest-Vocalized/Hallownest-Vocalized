de1e990059efa354936151bbe6c0f97628b5efd6
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2023-04-01 21:31:07 +0200

    fix video and audio there is no gameobject anymore

--- HKVocal/AssetBundleLoader/StyleLoader.cs
+++ HKVocal/AssetBundleLoader/StyleLoader.cs
@@ -5,14 +5,11 @@ namespace HKVocals;
 public static class StyleLoader
 {
     public static AssetBundle styleBundle;
-    public static GameObject styleGo;
     public static bool loaded = false;
 
     public static void LoadAssetBundle()
     {
         styleBundle = AssetBundle.LoadFromMemory(AssemblyUtils.GetBytesFromResources("Resources.stylebundle"));
-
-        styleGo = styleBundle.LoadAsset<GameObject>("HKVMenuVideo");
         loaded = true;
     }
 }
--- HKVocal/MajorFeatures/MenuTheme.cs
+++ HKVocal/MajorFeatures/MenuTheme.cs
@@ -27,13 +27,19 @@ public class MenuTheme : MonoBehaviour
         return orig;
     }
 
-    public static (string, GameObject, int, string, string[], MenuStyles.MenuStyle.CameraCurves, AudioMixerSnapshot) AddTheme(MenuStyles arg)
+    public static (string, GameObject, int, string, string[], MenuStyles.MenuStyle.CameraCurves, AudioMixerSnapshot) AddTheme(MenuStyles self)
     {
         //styleBundle ??= AssetBundle.LoadFromMemory(AssemblyUtils.GetBytesFromResources("Resources.stylebundle"));
         // GameObject go = Instantiate(styleBundle.LoadAsset<GameObject>("HKVMenuVideo"));
         GameObject go = new("HKVStyle");
+        go.SetActive(false);
+        go.transform.SetParent(self.gameObject.transform);
+        go.transform.localPosition = new Vector3(0, -1.2f, 0);
+
+        go.AddComponent<AudioSource>();
+        go.AddComponent<VideoPlayer>();
+        go.AddComponent<AudioSource>();
         go.AddComponent<MenuTheme>();
-        go.transform.parent = arg.transform;
 
         var cameraCurves = new MenuStyles.MenuStyle.CameraCurves()
         {
@@ -52,7 +58,10 @@ public class MenuTheme : MonoBehaviour
         cameraCurves.blueChannel.AddKey(new(0, 0));
         cameraCurves.blueChannel.AddKey(new(1, 1));
 
-        return ("STYLE_HKVOCALS", go, -1, "", null, cameraCurves, Resources.FindObjectsOfTypeAll<AudioMixer>().First(x => x.name == "Music").FindSnapshot("Silent")); // todo: replace snapshot with some none snapshot
+        DontDestroyOnLoad(go);
+        go.SetActive(true);
+
+        return ("STYLE_HKVOCALS", go, -1, "", null, cameraCurves, Resources.FindObjectsOfTypeAll<AudioMixer>().First(x => x.name == "Music").FindSnapshot("Silent"));
     }
 
     bool ready = false;
@@ -63,7 +72,7 @@ public class MenuTheme : MonoBehaviour
         yield return new WaitUntil(() => StyleLoader.loaded);
         ready = true;
 
-        var aSourceVid = gameObject.AddComponent<AudioSource>();
+        var aSourceVid = gameObject.GetComponents<AudioSource>()[0];
         aSourceVid.clip = null;
         aSourceVid.outputAudioMixerGroup = Resources.FindObjectsOfTypeAll<AudioMixerGroup>().First(x => x.name == "Atmos");
         aSourceVid.mute = false;
@@ -87,43 +96,44 @@ public class MenuTheme : MonoBehaviour
             new Keyframe(45.86174f, 1),
             new Keyframe(55.33846f, 0)
         }));
-        var vp = gameObject.AddComponent<VideoPlayer>();
-        vp.playOnAwake = false;
-        vp.audioOutputMode = VideoAudioOutputMode.AudioSource;
-        vp.renderMode = VideoRenderMode.CameraFarPlane;
-        vp.isLooping = true;
-        vp.targetCamera = GameCameras.instance.mainCamera;
-        vp.source = VideoSource.VideoClip;
-        vp.clip = StyleLoader.styleBundle.LoadAsset<VideoClip>("Sequence_03");
-        vp.SetTargetAudioSource(0, aSourceVid);
+        _video = gameObject.GetComponent<VideoPlayer>();
+        _video.playOnAwake = false;
+        _video.audioOutputMode = VideoAudioOutputMode.AudioSource;
+        _video.renderMode = VideoRenderMode.CameraFarPlane;
+        _video.isLooping = true;
+        _video.targetCamera = GameCameras.instance.mainCamera;
+        _video.source = VideoSource.VideoClip;
+        _video.clip = StyleLoader.styleBundle.LoadAsset<VideoClip>("Sequence_03");
+        _video.SetTargetAudioSource(0, aSourceVid);
+        DontDestroyOnLoad(_video.clip);
 
-        var aSourceMusic = gameObject.AddComponent<AudioSource>();
-        aSourceMusic.clip = StyleLoader.styleBundle.LoadAsset<AudioClip>("HV_Title_Card_music_compressed");
-        aSourceMusic.outputAudioMixerGroup = Resources.FindObjectsOfTypeAll<AudioMixer>().First(x => x.name == "Music").outputAudioMixerGroup;
-        aSourceMusic.mute = false;
-        aSourceMusic.bypassEffects = false;
-        aSourceMusic.bypassListenerEffects = false;
-        aSourceMusic.bypassReverbZones = false;
-        aSourceMusic.playOnAwake = false;
-        aSourceMusic.loop = false;
-        aSourceMusic.priority = 128;
-        aSourceMusic.volume = 1;
-        aSourceMusic.pitch = 1;
-        aSourceMusic.panStereo = 0;
-        aSourceMusic.spatialBlend = 0;
-        aSourceMusic.reverbZoneMix = 1;
-        aSourceMusic.dopplerLevel = 0;
-        aSourceMusic.spread = 0;
-        aSourceMusic.rolloffMode = AudioRolloffMode.Custom;
-        aSourceMusic.maxDistance = 58.79711f;
-        aSourceMusic.SetCustomCurve(AudioSourceCurveType.CustomRolloff, new AnimationCurve(new []
+        _audio = gameObject.GetComponents<AudioSource>()[1];
+        _audio.clip = StyleLoader.styleBundle.LoadAsset<AudioClip>("HV_Title_Card_music_compressed");
+        _audio.outputAudioMixerGroup = Resources.FindObjectsOfTypeAll<AudioMixer>().First(x => x.name == "Music").outputAudioMixerGroup;
+        _audio.mute = false;
+        _audio.bypassEffects = false;
+        _audio.bypassListenerEffects = false;
+        _audio.bypassReverbZones = false;
+        _audio.playOnAwake = false;
+        _audio.loop = false;
+        _audio.priority = 128;
+        _audio.volume = 1;
+        _audio.pitch = 1;
+        _audio.panStereo = 0;
+        _audio.spatialBlend = 0;
+        _audio.reverbZoneMix = 1;
+        _audio.dopplerLevel = 0;
+        _audio.spread = 0;
+        _audio.rolloffMode = AudioRolloffMode.Custom;
+        _audio.maxDistance = 58.79711f;
+        _audio.SetCustomCurve(AudioSourceCurveType.CustomRolloff, new AnimationCurve(new []
         {
             new Keyframe(45.86174f, 1),
             new Keyframe(55.33846f, 0)
         }));
+        DontDestroyOnLoad(_audio.clip);
 
-        StopAllCoroutines();
-        StartCoroutine(WaitForAudio());
+        OnEnable();
     }
 
     private void OnEnable()
@@ -131,18 +141,7 @@ public class MenuTheme : MonoBehaviour
         if (ready)
         {
             _video.Play();
-            if (!finished)
-            {
-                _audio.Play();
-                StopAllCoroutines();
-                StartCoroutine(WaitForAudio());
-            }
+            _audio.Play();
         }
-    }
-
-    private IEnumerator WaitForAudio()
-    {
-        yield return new WaitWhile(() => _audio.isPlaying);
-        finished = true;
     }
 }
